6685843a06718f2fdc3f3ce6bfcc98d2aa362bb5
azat <khairov.azat@gmail.com>
2021-11-20 21:36:24 +0600

    best try of creating benches

--- benches/engine_perf.rs
+++ benches/engine_perf.rs
@@ -4,9 +4,10 @@ use kvs::engine::*;
 use rand::distributions::Alphanumeric;
 use rand::prelude::*;
 use rand_pcg::Pcg64;
+use std::collections::HashMap;
+use std::fs;
 use std::path::PathBuf;
 use tempfile::TempDir;
-
 #[derive(Clone)]
 struct EngineHolder {
     lkvs: Option<LogStructKVStore>,
@@ -51,35 +52,48 @@ fn generate_random_string(seed: u64) -> String {
 
 fn set_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("set_bench");
-    for engine in [EngineType::Sled, EngineType::Kvs].iter() {
-        group.bench_with_input(BenchmarkId::from_parameter(engine), engine, |b, engine| {
-            b.iter_batched(
-                || {
-                    let temp_dir = TempDir::new().unwrap();
-                    let mut kv_store = match engine {
-                        EngineType::Kvs => EngineHolder {
-                            lkvs: Some(LogStructKVStore::open(&temp_dir.path()).unwrap()),
-                            sled: None,
-                            engine_type: EngineType::Kvs,
-                        },
-                        EngineType::Sled => EngineHolder {
-                            lkvs: None,
-                            sled: Some(SledStore::open(&temp_dir.path()).unwrap()),
-                            engine_type: EngineType::Sled,
-                        },
-                    };
-                    (kv_store, temp_dir)
-                },
-                |(mut kv_store, temp_dir)| {
-                    for seed in (0..200).step_by(2) {
-                        let key = generate_random_string(seed);
-                        let value = generate_random_string(seed + 1);
-                        kv_store.set(key, value);
-                    }
-                },
-                BatchSize::LargeInput,
-            );
-        });
+    for engine in [EngineType::Kvs, EngineType::Sled].iter() {
+        let temp_dir = TempDir::new().unwrap();
+        let mut kv_store = match engine {
+            EngineType::Kvs => EngineHolder {
+                lkvs: Some(LogStructKVStore::open(temp_dir.path()).unwrap()),
+                sled: None,
+                engine_type: EngineType::Kvs,
+            },
+            EngineType::Sled => EngineHolder {
+                lkvs: None,
+                sled: Some(SledStore::open(temp_dir.path()).unwrap()),
+                engine_type: EngineType::Sled,
+            },
+        };
+
+        group.bench_with_input(
+            BenchmarkId::from_parameter(engine),
+            &kv_store,
+            |b, kv_store| {
+                b.iter_batched(
+                    || {
+                        let mut keys = Vec::new();
+                        let mut values = Vec::new();
+
+                        let mut rng = Pcg64::seed_from_u64(1);
+
+                        for _ in 0..2000 {
+                            keys.push(rng.gen_range(0..100).to_string());
+                            values.push(rng.gen_range(0..100).to_string());
+                        }
+
+                        (kv_store, keys, values)
+                    },
+                    |(mut kv_store, mut keys, mut values)| {
+                        for _ in 0..keys.len() {
+                            kv_store.set(keys.pop().unwrap(), values.pop().unwrap());
+                        }
+                    },
+                    BatchSize::LargeInput,
+                );
+            },
+        );
     }
     group.finish();
 }
@@ -87,39 +101,47 @@ fn set_bench(c: &mut Criterion) {
 fn get_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("get_bench");
     for engine in [EngineType::Sled, EngineType::Kvs].iter() {
-        group.bench_with_input(BenchmarkId::from_parameter(engine), engine, |b, engine| {
-            b.iter_batched(
-                || {
-                    let temp_dir = TempDir::new().unwrap();
-                    let mut kv_store = match engine {
-                        EngineType::Kvs => EngineHolder {
-                            lkvs: Some(LogStructKVStore::open(&temp_dir.path()).unwrap()),
-                            sled: None,
-                            engine_type: EngineType::Kvs,
-                        },
-                        EngineType::Sled => EngineHolder {
-                            lkvs: None,
-                            sled: Some(SledStore::open(&temp_dir.path()).unwrap()),
-                            engine_type: EngineType::Sled,
-                        },
-                    };
-                    (kv_store, temp_dir)
-                },
-                |(mut kv_store, temp_dir)| {
-                    for seed in (200..400).step_by(2) {
-                        let key = generate_random_string(seed);
-                        let value = generate_random_string(seed + 1);
-                        kv_store.set(key, value);
-                    }
-                    for seed in (200..400).step_by(2) {
-                        let key = generate_random_string(seed);
-                        let value = generate_random_string(seed + 1);
-                        assert_eq!(value, kv_store.get(key).unwrap().unwrap());
-                    }
-                },
-                BatchSize::LargeInput,
-            );
-        });
+        let temp_dir = TempDir::new().unwrap();
+        let mut kv_store = match engine {
+            EngineType::Kvs => EngineHolder {
+                lkvs: Some(LogStructKVStore::open(&temp_dir.path()).unwrap()),
+                sled: None,
+                engine_type: EngineType::Kvs,
+            },
+            EngineType::Sled => EngineHolder {
+                lkvs: None,
+                sled: Some(SledStore::open(&temp_dir.path()).unwrap()),
+                engine_type: EngineType::Sled,
+            },
+        };
+        group.bench_with_input(
+            BenchmarkId::from_parameter(engine),
+            &kv_store,
+            |b, kv_store| {
+                b.iter_batched(
+                    || {
+                        let mut index = HashMap::<String, String>::new();
+                        let mut rng = Pcg64::seed_from_u64(1);
+
+                        for _ in 0..2000 {
+                            let key = rng.gen_range(0..100).to_string();
+                            let value = rng.gen_range(0..100).to_string();
+                            index.insert(key.clone(), value.clone());
+
+                            kv_store.set(key, value);
+                        }
+
+                        (kv_store, index)
+                    },
+                    |(mut kv_store, mut index)| {
+                        for (key, value) in index.iter() {
+                            assert_eq!(value.clone(), kv_store.get(key.clone()).unwrap().unwrap());
+                        }
+                    },
+                    BatchSize::LargeInput,
+                );
+            },
+        );
     }
     group.finish();
 }
--- benches/pool_perf.rs
+++ benches/pool_perf.rs
@@ -10,15 +10,18 @@ use kvs::thread_pool::*;
 use rand::distributions::Alphanumeric;
 use rand::prelude::*;
 use rand_pcg::Pcg64;
+use std::collections::HashMap;
+use std::env;
+use std::fs;
 use std::net::SocketAddr;
 use std::rc::Rc;
-use std::sync::Arc;
+use std::sync::{Arc, Barrier};
 use std::thread;
 use std::time::Duration;
 use tempfile::TempDir;
-
 fn generate_random_string(seed: u64) -> String {
     let mut rng = Pcg64::seed_from_u64(seed);
+
     let len: usize = rng.gen_range(1..10000);
     let s: String = rng
         .sample_iter(&Alphanumeric)
@@ -32,35 +35,42 @@ fn generate_random_string(seed: u64) -> String {
 fn pool_set(c: &mut Criterion) {
     let mut group = c.benchmark_group("pool_set");
     group
-        .measurement_time(Duration::from_millis(4000))
+        .measurement_time(Duration::from_millis(6000))
         .warm_up_time(Duration::from_millis(1));
 
-    for i in [4, 8].iter() {
+    for i in [1, 2, 4, 6, 8] {
         let temp_dir = TempDir::new().unwrap();
-        let mut pool = RayonThreadPool::new(i.clone() as u32).unwrap();
-        let mut kv_store = SledStore::open(temp_dir.path()).unwrap();
-
-        let mut keys = Vec::new();
-        let mut values = Vec::new();
-
-        for i in (0..1000).step_by(2) {
-            keys.push(generate_random_string(i));
-            values.push(generate_random_string(i + 1));
-        }
-
-        group.bench_function(
+        let mut kv_store = LogStructKVStore::open(temp_dir.path()).unwrap();
+        group.bench_with_input(
             BenchmarkId::from_parameter(format!("Num cpus: #{}", i)),
-            |b| {
-                b.iter(black_box(|| {
-                    for i in 0..keys.len() {
-                        let key = keys.pop().unwrap();
-                        let value = values.pop().unwrap();
-                        let kv_store = kv_store.clone();
-                        pool.spawn(move || {
-                            kv_store.set(key, value);
-                        });
-                    }
-                }));
+            &(i, kv_store),
+            |b, (i, kv_store)| {
+                b.iter_batched(
+                    || {
+                        let mut keys = Vec::new();
+                        let mut values = Vec::new();
+                        let mut rng = thread_rng();
+
+                        for _ in 0..100 {
+                            keys.push(rng.gen_range(0..100).to_string());
+                            values.push(rng.gen_range(0..100).to_string());
+                        }
+                        let mut pool = SharedQueueThreadPool::new(i.clone() as u32).unwrap();
+
+                        (keys, values, pool)
+                    },
+                    |(mut keys, mut values, mut pool)| {
+                        for i in 0..keys.len() {
+                            let key = keys.pop().unwrap();
+                            let value = values.pop().unwrap();
+                            let kv_store = kv_store.clone();
+                            pool.spawn(move || {
+                                kv_store.set(key, value).unwrap();
+                            });
+                        }
+                    },
+                    BatchSize::SmallInput,
+                );
             },
         );
     }
